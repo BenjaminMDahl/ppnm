17edb6d236d06234013086e46796675efaed6142
Benjamin <201708999@post.au.dk>
2021-02-26 11:27:30 +0100

    Initial commit

---
+++ exercises/gnuplot/gamma/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include<gsl/gsl_sf_erf.h>
+
+#include"mygamma.h"
+
+
+int main(){
+	double xmin=1,xmax=9;
+	for(double x=xmin;x<=xmax;x=x+1){
+		printf("%10g %10g %i %10g\n",x,tgamma(x),gsl_sf_gamma(x),mygamma(x));
+		}
+
+return 0;
+}
