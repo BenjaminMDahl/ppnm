d69b28969eeca029ab4b48ea67370de66ccf4e5d
Benjamin <201708999@post.au.dk>
2021-03-11 14:31:57 +0100

    Initial commit

--- exercises/Muliprocessing/numbers.c
+++ exercises/Muliprocessing/numbers.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include<pthread.h>
+#include<omp.h>
+
 
 int main(){
 	int n=10000;
-	double cirkelx[n];
-	double cirkely[n];
-	double x[n];
-	double y[n];
 
-	for (int i=0; i <n; i++){
-		x[i]=cos(rand());
-	}
-	for (int i=0; i <n; i++){
-		y[i]=cos(rand());
-	}
-	for (int i=0; i <n; i++){
-		cirkelx[i]=cos(3.14*2*i/n);
-		cirkely[i]=sin(3.14*2*i/n);
+	double x[n], y[n];
+#pragma omp parallel sections
+	{
+	#pragma omp section
+		{
+		for (int i=0; i <n; i++) x[i]= (double)rand() / (double)RAND_MAX;
+		}
+	#pragma omp section
+		{
+		for (int i=0; i <n; i++) y[i]= (double)rand() / (double)RAND_MAX;
+		}
 	}
 
 	int N_in=0;
-
 	for(int i=0; i<n; i++){
 		if(sqrt(x[i]*x[i]+y[i]*y[i])<=1){
 			 N_in=N_in+1;
 			}
 				}
-	double pi=4*N_in/n;
-
+	double pi=4*(double)N_in/(double)n;
 
 	for(int i=0; i<n; i++){
-		printf("%g %g %g %g\n",x[i],y[i],cirkelx[i],cirkely[i]);
+		printf("%g %g\n",x[i],y[i]);
 		}
 
-	fprintf(,"PI=%g\n",pi);
+	FILE * Pi;
+	Pi = fopen ("file.txt", "w+");
+	fprintf(Pi,"Pi=%g\n",pi);
+	fclose(Pi);
+
+
 return 0;
 }
