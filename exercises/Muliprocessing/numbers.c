7e24bd3fa4488fe81b1fb015338869b7af1c5063
Benjamin <201708999@post.au.dk>
2021-03-11 11:05:06 +0100

    Initial commit

---
+++ exercises/Muliprocessing/numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<pthread.h>
+
+int main(){
+	int n=100;
+	double cirkelx[n];
+	double cirkely[n];
+	double x[n];
+	double y[n];
+
+	for (int i=0; i <n; i++){
+		x[i]=fabs(cos(rand()));
+	}
+	for (int i=0; i <n; i++){
+		y[i]=fabs(cos(rand()));
+	}
+	for (int i=0; i <n; i++){
+		cirkelx[i]=cos(3.14/2*i/n);
+		cirkely[i]=sin(3.14/2*i/n);
+	}
+
+	for(int i=0; i<n; i++){
+		printf("%g %g %g %g\n",x[i],y[i],cirkelx[i],cirkely[i]);
+		}
+
+return 0;
+}
