fd189a1c2add5239c89410d2846c2d1c03cb3554
Benjamin <201708999@post.au.dk>
2021-03-15 11:04:07 +0100

    Initial commit

--- exercises/latex/main.c
+++ exercises/latex/main.c
@@ -9,8 +9,8 @@ return 1+x*(1+x/2*(1+x/3*(1+x/4*(1+x/5*(1+x/6*(1+x/7*(1+x/8*(1+x/9*(1+x/10))))))
 }
 
 int main(){
-	for(int i=0;i<101;i++){
-	double x= i/10;
+	for(int i=-100;i<101;i++){
+	double x= (double)i/10;
 	printf("%g %g %g\n",x,exp(x),ex(x));
 	}
 return 0;
