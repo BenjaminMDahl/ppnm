fe3350e5c91d237fcfd7700dadce62d3144c6a58
Benjamin <201708999@post.au.dk>
2021-04-15 10:23:36 +0200

    Initial commit

---
+++ homework/AdaptiveIntegration/gsl_fun.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+#include<gsl/gsl_integration.h>
+
+struct my_f_params { double a; };
+
+double fun(double x, void* p){
+	struct my_f_params * params = (struct my_f_params *)p;
+	double a= (params->a);
+	double y= a*sqrt(1-x*x);
+
+return y;
+}
+
+double gsl_int(double a, double b, double delta, double eps){
+
+	gsl_integration_workspace * w  = gsl_integration_workspace_alloc (1000);
+	double result, error;
+	struct my_f_params par= {4.0};
+
+	gsl_function F;
+	F.function = fun;
+ 	F.params= &par;
+
+
+	gsl_integration_qags (&F, a, b, delta, eps, 1000,
+                        w, &result, &error);
+
+	gsl_integration_workspace_free (w);
+return result;
+}
--- homework/AdaptiveIntegration/main.c
+++ homework/AdaptiveIntegration/main.c
@@ -14,6 +14,9 @@ double inv_kvrod_cc(double x);
 double ln_kvrod(double x);
 double ln_kvrod_cc(double x);
 
+double gsl_int(double f(double), double a, double b, double delta, double eps);
+
+
 int main(){
 
 double Q1=integrate(kvrod,0,1,0.0001,0.0001);
@@ -24,14 +27,14 @@ printf("Det analytiske svar er\n\n %10g\n",ana);
 
 double Q2=integrate(kvrod_2,0,1,0.00001,0.00001);
 double Q2_CC=integrate(kvrod_2_cc,0,M_PI,0.00001,0.00001);
-//double Q2_GSL=
+double Q2_GSL=gsl_int(kvrod_2,0,1,0.00001,0.00001);
 
 
-printf("Mit numeriske integral af 4*sqrt(1-x^2) fra 0 til 1 giver\n %30g\n",Q2);
-printf("Når man bruger CC transformation på det oversteånde får man \n %30g \n",Q2_CC);
-//printf("Fra GSL får man\n %30g \n",Q2_GSL);
-printf("Det analytiske svar er\n %10g\n\n",M_PI);
-
+printf("Mit numeriske integral af 4*sqrt(1-x^2) fra 0 til 1 giver\n %.17g\n",Q2);
+printf("Når man bruger CC transformation på det oversteånde får man \n %.17g \n",Q2_CC);
+printf("Fra GSL får man\n %.17g \n",Q2_GSL);
+printf("Det analytiske svar er\n %.17g\n\n",M_PI);
+printf("Det ses at når de alle er underlagt samme tolerance krav, kan GSL få op til 15 cifre rigtigt,\n mens mine operationer højst giver 8 cifre og kun 7 når man ikke bruger CC transformation.\n\n");
 
 double Q3=integrate(inv_kvrod,0,1,0.001,0.001);
 double Q3_CC=integrate(inv_kvrod_cc,0,M_PI,0.001,0.001);
