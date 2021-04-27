3f5740ecb5c10e41c5f7a20bcb3532e899c3c374
Benjamin <201708999@post.au.dk>
2021-04-27 10:57:15 +0200

    Initial commit

--- homework/Roots/exact.c
+++ homework/Roots/exact.c
@@ -2,10 +2,10 @@
 #include<math.h>
 
 int main(){
-int n=801;
+int n=8001;
 double x[n],y[n];
 for(int i=0;i<n;i++){
-x[i]=i/100;
+x[i]=i/1000;
 y[i]=x[i]*exp(-x[i]);
 printf("%6g %6g\n",x[i],y[i]);}
 }
