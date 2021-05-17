bac6fa7dddef27ee83f96385f968b052f9b52966
Benjamin <201708999@post.au.dk>
2021-05-17 14:40:51 +0200

    Initial commit

--- homework/ArtificialNerualNetwork/main.c
+++ homework/ArtificialNerualNetwork/main.c
@@ -3,15 +3,15 @@
 #include<gsl/gsl_vector.h>
 #include"network.h"
 
-double activation_function(double x){return exp(-x*x);} // Gaussian
+double activation_function(double x){return x*exp(-x*x);} // Gaussian
 double function_to_fit(double x){return sin(x)*sin(x)/x;}
 int main(){
 	int n=4;
 	ann* network=ann_alloc(n,activation_function);
 
 	//Laver tabulated values baseret på function to fit
-	double a=-4,b=4;
-	int N=50;
+	double a=-8,b=8;
+	int N=30;
 	gsl_vector* vx=gsl_vector_alloc(N);
 	gsl_vector* vy=gsl_vector_alloc(N);
 
