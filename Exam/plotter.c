7d13a1c6700da147067ee3fb84e8d3fafe32547e
Benjamin <201708999@post.au.dk>
2021-06-24 19:53:30 +0200

    Initial commit

--- Exam/plotter.c
+++ Exam/plotter.c
@@ -20,6 +20,8 @@ return S;
 
 int main(){
 
+
+	////plotning af egenværdier////
 int n=5;
 gsl_vector* D=gsl_vector_alloc(n);
 gsl_vector* u=gsl_vector_alloc(n);
@@ -31,6 +33,7 @@ for(int i=0;i<n;i++){
 	gsl_vector_set(u,i,tal[i]);}
 	secular_equation_default(D,u,p,x,0.00001,0);
 
+// Først dataene til resultatet fra vores funktion
 printf("#index 1:lambda_i     f(lambda_i)\n");
 for(int i=0;i<x->size;i++){
 	double xi=gsl_vector_get(x,i);
@@ -39,11 +42,44 @@ for(int i=0;i<x->size;i++){
 
 
 printf("\n\n");
-//Derefter til kurve
+//Derefter data til at lave det karakteristiske polynomium
 printf("#index 2:x_i    f(x_i)\n");
 for(double i=-2;i<15;i=i+0.11){
 	double fi=plot_data(i);
 	printf("%10g %10g\n",i,fi);}
+printf("\n\n");
 
+	////plotning af effektivitetten af default som funktion af m////
+
+printf("#index 3: Data for default effektivitet som funktion af m\n");
+int L=1000;
+gsl_vector* D1001=gsl_vector_alloc(L);
+gsl_vector* u1001=gsl_vector_alloc(L);
+gsl_vector* x1001=gsl_vector_alloc(L);
+gsl_vector* D1005=gsl_vector_alloc(L);
+gsl_vector* u1005=gsl_vector_alloc(L);
+gsl_vector* x1005=gsl_vector_alloc(L);
+gsl_vector* D1100=gsl_vector_alloc(L);
+gsl_vector* u1100=gsl_vector_alloc(L);
+gsl_vector* x1100=gsl_vector_alloc(L);
+for(int i=0;i<L;i++){
+	gsl_vector_set(D1001,i,RND);
+	gsl_vector_set(u1001,i,RND);
+	gsl_vector_set(D1005,i,RND*5);
+	gsl_vector_set(u1005,i,RND*5);
+	gsl_vector_set(D1100,i,RND*100);
+	gsl_vector_set(u1100,i,RND*100);}
+for(int i=0;i<=100;i=i+5){
+	int eig1=0;int eig5=0;int eig100=0;
+	secular_equation_default(D1001,u1001,25,x1001,0.0001,i);
+	secular_equation_default(D1005,u1005,25,x1005,0.0001,i);
+	secular_equation_default(D1100,u1100,25,x1100,0.0001,i);
+	for(int j=0;j<L;j++){
+	if(isnan(gsl_vector_get(x1001,j)))eig1++;
+	if(isnan(gsl_vector_get(x1005,j)))eig5++;
+	if(isnan(gsl_vector_get(x1100,j)))eig100++;
+	}
+	printf("%4i %4i %4i %4i\n",i,L-eig1,L-eig5,L-eig100);
+	}
 return 0;
 }
